30cb5049656530f13aad9d3f3859229feacedb66
ccalderon117 <53982609+bga-admin@users.noreply.github.com>
2022-09-02 14:19:24 -0500

    Merge

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Body from "./Components/Body"
 
 function App() {
   return (
-    <div >
+    <div className='WORK'>
             <Navbar />
       <Body />
       <header className="App-header">
